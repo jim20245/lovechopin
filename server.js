a330a7c715cbe605ac598b9a67c8726ddcb6630a
jamydino <18064908797@163.com>
2025-12-13 23:53:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization: Bearer BAIDU_API_KEY');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
